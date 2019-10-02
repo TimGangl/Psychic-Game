40e3b0b3d1c8626d19a0518acad420ab5d74ee7a
Tim Gangl <tim.gangl30@gmail.com>
2019-10-02 15:43:01 -0700

    adding picture remove css

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+// Variables
+var wins = 0;
+var loses = 0;
+var guessesLeft = 10;
+
+var letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+// code for wins
+
+//code for losses
+
+// code for guesses left
+
+//code to randomly pick number
+
+// code to console.log letters guessed
---
+++ assets/javascript/practice/game.js
@@ -0,0 +1,10 @@
+const x = 4;
+const y = 10;
+
+if (x > 5 || y > 10) {
+  console.log("x is 10");
+} else if (x > 10) {
+  console.log("x is greater than 10");
+} else {
+  console.log("x is not 10");
+}
