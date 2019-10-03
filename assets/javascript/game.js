c65566bff3c1e44a22d532c18d3a5da1802af42b
Tim Gangl <tim.gangl30@gmail.com>
2019-10-02 19:43:27 -0700

    Working random letter code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,35 +2,10 @@
 var wins = 0;
 var loses = 0;
 var guessesLeft = 10;
+var randomLetter = "";
 
-var letters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z"
-];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // code for wins
 
 //code for losses
@@ -38,5 +13,10 @@ var letters = [
 // code for guesses left
 
 //code to randomly pick number
-
+function genLetter(array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+randomLetter = genLetter(letters);
+console.log(randomLetter);
 // code to console.log letters guessed
+//for (let i = 0; i < ; i++);
